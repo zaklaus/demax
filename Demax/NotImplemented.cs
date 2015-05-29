75d1387ddf29a8946bbfd7132392dda526d380b0
Dominik Madarasz <combatwz.sk@gmail.com>
2015-05-29 22:08:21 +0200

    Updated License

--- Demax/NotImplemented.cs
+++ Demax/NotImplemented.cs
@@ -26,85 +26,14 @@
 //  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 //
 using System;
-using System.Collections.Generic;
 
 namespace Demax
 {
-	/// <summary>
-	/// Entity manager.
-	/// </summary>
-	public class CEntityManager
+	public class NotImplemented
 	{
-		List<CEntity> entities;
-
-		/// <summary>
-		/// Initializes a new instance of the <see cref="Demax.CEntityManager"/> class.
-		/// </summary>
-		public CEntityManager ()
+		public NotImplemented ()
 		{
-			entities = new List<CEntity> ();
-		}
-
-		/// <summary>
-		/// Creates the entity.
-		/// </summary>
-		/// <param name="name">Name.</param>
-		/// <param name="tag">Tag.</param>
-		public CEntity CreateEntity(string name="NewEntity",string tag="")
-		{
-			CEntity e = new CEntity(name,tag);
-			entities.Add(e);
-			Console.WriteLine ("Entity: " + name);
-
-			return e;
-		}
-
-		/// <summary>
-		/// Gets the entities.
-		/// </summary>
-		/// <returns>The entities.</returns>
-		public List<CEntity> GetEntities()
-		{
-			return entities;
-		}
-
-		/// <summary>
-		/// Removes the entity.
-		/// </summary>
-		/// <param name="e">E.</param>
-		public void RemoveEntity(CEntity e)
-		{
-			entities.Remove(e);
-		}
-
-		/// <summary>
-		/// Gets the entity.
-		/// </summary>
-		/// <returns>The entity.</returns>
-		/// <param name="id">Identifier.</param>
-		public CEntity GetEntity(int id)
-		{
-			if (id < entities.Count && id >= 0) {
-				return entities [id];
-			}
-
-			return null;
-		}
-
-		/// <summary>
-		/// Gets the name of the entity by.
-		/// </summary>
-		/// <returns>The entity by name.</returns>
-		/// <param name="name">Name.</param>
-		public CEntity GetEntityByName(string name)
-		{
-			foreach (CEntity e in entities) {
-				if (e.Name == name) {
-					return e;
-				}
-			}
-
-			return null;
+			throw new NotImplementedException();
 		}
 	}
 }
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # demax
 Demax 3D Game Engine
+
+Demax is simplistic school project focused on 3D rendering. It contains working hierarchy system. Shader system (with a few example shaders) and working IronPython implementation.
+
+### TODO:
+- Implement sound
+- Optimize, optimize, optimize...
+- Develop basic level editor
+- Implement Wavefront OBJ loader (or any model-parsing plugins)
+- Create documentation
+- Remove uneccessary code
+- Implement network code (looking forward to it)
+- Remodel rendering system
+- A lot more...
+
+### Copyright
+Read LICENSE for licensing info.
